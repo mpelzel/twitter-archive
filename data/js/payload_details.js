bbd444f560b7d78283d3aa199d5939701c0f5fb2
Morris Pelzel <morris.pelzel@gmail.com>
2018-10-05 19:28:11 -0500

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 6961,
-	"created_at": "Fri Oct 05 00:19:59 +0000 2018",
+	"tweets": 6962,
+	"created_at": "Sat Oct 06 00:28:10 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,239 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Fri Oct 05 03:01:30 +0000 2018",
+		"id": 1048045487494979600,
+		"id_str": "1048045487494979584",
+		"text": "RT @desmoinesdem: ICYMI, Waterloo teacher and activist @JonathanGrieder is one scared young man: https://t.co/z2VuMRB8Ur",
+		"truncated": false,
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "desmoinesdem",
+					"name": "Bleeding Heartland",
+					"id": 19785310,
+					"id_str": "19785310",
+					"indices": [
+						3,
+						16
+					]
+				},
+				{
+					"screen_name": "JonathanGrieder",
+					"name": "Jonathan Grieder",
+					"id": 66510637,
+					"id_str": "66510637",
+					"indices": [
+						55,
+						71
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/z2VuMRB8Ur",
+					"expanded_url": "https://www.bleedingheartland.com/2018/10/03/scared-young-man/",
+					"display_url": "bleedingheartland.com/2018/10/03/sca…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 33972737,
+			"id_str": "33972737",
+			"name": "Mo Pelzel",
+			"screen_name": "MorrisPelzel",
+			"location": "Grinnell, Iowa",
+			"description": "Director of Academic Technology @GrinnellCollege | online theology prof @Creighton | husband | dad | trail runner | cyclist | cantor | Texan | puzzle/games geek",
+			"url": "https://t.co/HifqlQpaOV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HifqlQpaOV",
+							"expanded_url": "http://morrispelzel.com",
+							"display_url": "morrispelzel.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 651,
+			"friends_count": 1003,
+			"listed_count": 38,
+			"created_at": "Tue Apr 21 17:29:45 +0000 2009",
+			"favourites_count": 5607,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6867,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/428212615102226432/HN8-wYRh_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/428212615102226432/HN8-wYRh_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/33972737/1431025149",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"retweeted_status": {
+			"created_at": "Fri Oct 05 00:48:21 +0000 2018",
+			"id": 1048011979737260000,
+			"id_str": "1048011979737260033",
+			"text": "ICYMI, Waterloo teacher and activist @JonathanGrieder is one scared young man: https://t.co/z2VuMRB8Ur",
+			"truncated": false,
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "JonathanGrieder",
+						"name": "Jonathan Grieder",
+						"id": 66510637,
+						"id_str": "66510637",
+						"indices": [
+							37,
+							53
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/z2VuMRB8Ur",
+						"expanded_url": "https://www.bleedingheartland.com/2018/10/03/scared-young-man/",
+						"display_url": "bleedingheartland.com/2018/10/03/sca…",
+						"indices": [
+							79,
+							102
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 19785310,
+				"id_str": "19785310",
+				"name": "Bleeding Heartland",
+				"screen_name": "desmoinesdem",
+				"location": "Windsor Heights, Iowa",
+				"description": "Reporter covering Iowa politics for the progressive website Bleeding Heartland. Has been called ornery by one state lawmaker and a traitor by another.",
+				"url": "http://t.co/vkjjXRRVhN",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/vkjjXRRVhN",
+								"expanded_url": "http://www.bleedingheartland.com/",
+								"display_url": "bleedingheartland.com",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 9274,
+				"friends_count": 1238,
+				"listed_count": 472,
+				"created_at": "Fri Jan 30 20:06:08 +0000 2009",
+				"favourites_count": 64821,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 77193,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/74441462/logo_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/74441462/logo_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 9,
+			"favorite_count": 15,
+			"favorited": false,
+			"retweeted": true,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en"
+	},
+	{
 		"created_at": "Thu Oct 04 16:00:50 +0000 2018",
 		"id": 1047879226739904500,
 		"id_str": "1047879226739904512",
