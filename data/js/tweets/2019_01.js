eca42e8c96374c6959d5fb405c8da31bdc07ebbc
Morris Pelzel <morris.pelzel@gmail.com>
2019-01-26 18:20:01 -0600

    Added 1 tweets

--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 26 16:26:07 +0000 2019",
+		"id": 1089197806265475100,
+		"id_str": "1089261425242136578",
+		"full_text": "\"The same liberals who fret about “micro-aggressions” for 20-somethings were able to see 16-year-olds subjected to the worst racist garbage from religious bigots … and then express the desire to punch the kids in the face.\"\n\n https://t.co/gUtCDMNumt via @intelligencer",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "intelligencer",
+					"name": "Intelligencer",
+					"id": 45565185,
+					"id_str": "45565185",
+					"indices": [
+						254,
+						268
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/gUtCDMNumt",
+					"expanded_url": "http://nymag.com/intelligencer/2019/01/andrew-sullivan-the-abyss-of-hate-versus-hate.html?utm_source=tw",
+					"display_url": "nymag.com/intelligencer/…",
+					"indices": [
+						226,
+						249
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 331816614,
+			"id_str": "331816614",
+			"name": "Andrew Sullivan",
+			"screen_name": "sullydish",
+			"location": "Washington, DC",
+			"description": "Here for the articles.",
+			"url": "https://t.co/TOlhy6dXZq",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TOlhy6dXZq",
+							"expanded_url": "http://dish.andrewsullivan.com",
+							"display_url": "dish.andrewsullivan.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 138116,
+			"friends_count": 104,
+			"listed_count": 3588,
+			"created_at": "Fri Jul 08 19:20:14 +0000 2011",
+			"favourites_count": 240,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7736,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/3205562537/1ccdefbf621cd2d56ea6cabb700f6a3c_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/3205562537/1ccdefbf621cd2d56ea6cabb700f6a3c_normal.png",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 654,
+		"favorite_count": 1859,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"The same liberals who fret about “micro-aggressions” for 20-somethings were able to see 16-year-olds subjected to the worst racist garbage from religious bigots … and then express the desire to punch the kids in the face.\"\n\n https://t.co/gUtCDMNumt via @intelligencer"
+	},
+	{
 		"created_at": "Fri Jan 25 19:38:27 +0000 2019",
 		"id": 1088883818453454800,
 		"id_str": "1088883818453454848",
