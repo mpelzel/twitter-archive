a3377f34d836bb5f26438650a41421eba1feca16
Morris Pelzel <morris.pelzel@gmail.com>
2019-02-01 18:20:03 -0600

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 8079,
-	"created_at": "Fri Feb 01 00:20:02 +0000 2019",
+	"tweets": 8084,
+	"created_at": "Sat Feb 02 00:20:01 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_02.js
@@ -0,0 +1,598 @@
+Grailbird.data.tweets_2019_02 = 
+[
+	{
+		"created_at": "Fri Feb 01 23:16:22 +0000 2019",
+		"id": 1091475373961797600,
+		"id_str": "1091476176017809408",
+		"full_text": "Fellow Catholics, here is our calling. I'm so grateful for @americamag editor @OlgaMSegura's report on how the church must learn from #blacklivesmatter: What Black Lives Matter can teach Catholics about racial justice https://t.co/mYkWspODxF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "blacklivesmatter",
+					"indices": [
+						134,
+						151
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "americamag",
+					"name": "America Magazine",
+					"id": 17372204,
+					"id_str": "17372204",
+					"indices": [
+						59,
+						70
+					]
+				},
+				{
+					"screen_name": "OlgaMSegura",
+					"name": "Olga Marina Segura",
+					"id": 928409251,
+					"id_str": "928409251",
+					"indices": [
+						78,
+						90
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mYkWspODxF",
+					"expanded_url": "https://www.americamagazine.org/politics-society/2019/02/01/what-black-lives-matter-can-teach-catholics-about-racial-justice",
+					"display_url": "americamagazine.org/politics-socie…",
+					"indices": [
+						218,
+						241
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19619556,
+			"id_str": "19619556",
+			"name": "Nathan Schneider",
+			"screen_name": "ntnsndr",
+			"location": "Colorado",
+			"description": "Profsr: @CUBoulderMDST / Authr: #Coop #Occupy #God / Regulr: @AmericaMag @YesMagazine / Reportr: @TheNation @ChronicleReview @VICE / Organizr: #platformcoop",
+			"url": "https://t.co/wk9eH3IIPB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/wk9eH3IIPB",
+							"expanded_url": "https://nathanschneider.info",
+							"display_url": "nathanschneider.info",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8430,
+			"friends_count": 1678,
+			"listed_count": 486,
+			"created_at": "Tue Jan 27 21:58:28 +0000 2009",
+			"favourites_count": 13159,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 15654,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme2/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/938928648098258944/UvY1UlF5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/938928648098258944/UvY1UlF5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/19619556/1525364271",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DAECF4",
+			"profile_text_color": "43403D",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fellow Catholics, here is our calling. I'm so grateful for @americamag editor @OlgaMSegura's report on how the church must learn from #blacklivesmatter: What Black Lives Matter can teach Catholics about racial justice https://t.co/mYkWspODxF"
+	},
+	{
+		"created_at": "Fri Feb 01 22:48:29 +0000 2019",
+		"id": 1091468357034041300,
+		"id_str": "1091469174424117249",
+		"full_text": "Collaboratively created History of the Information Age syllabus (with readings/materials to come) ready https://t.co/uOxxvTfWaR #InfoAge19",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			138
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "InfoAge19",
+					"indices": [
+						128,
+						138
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/uOxxvTfWaR",
+					"expanded_url": "https://courses.mcclurken.org/427/draft-syllabus-v-1-0/",
+					"display_url": "courses.mcclurken.org/427/draft-syll…",
+					"indices": [
+						104,
+						127
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 6108882,
+			"id_str": "6108882",
+			"name": "Jeffrey McClurken",
+			"screen_name": "jmcclurken",
+			"location": "Virginia",
+			"description": "Chief of Staff to UMW @PresTPain & Prof of History/AMST. Digital History Reviews Ed for @JournAmHist (Former Dept Chair & Spec Asst to the Provost)",
+			"url": "https://t.co/gJ19u3mdsk",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/gJ19u3mdsk",
+							"expanded_url": "http://mcclurken.org",
+							"display_url": "mcclurken.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 3854,
+			"friends_count": 1964,
+			"listed_count": 345,
+			"created_at": "Thu May 17 14:25:46 +0000 2007",
+			"favourites_count": 9739,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 24745,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/866811823256948737/USIZnFp1_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/866811823256948737/USIZnFp1_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/6108882/1487343722",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Collaboratively created History of the Information Age syllabus (with readings/materials to come) ready https://t.co/uOxxvTfWaR #InfoAge19"
+	},
+	{
+		"created_at": "Fri Feb 01 22:24:41 +0000 2019",
+		"id": 1091462368549322800,
+		"id_str": "1091463051658887169",
+		"full_text": "Hot off the press! #DLINQdigdetox 2.8 - Universal Design for Learning &amp; Accessibility, by @billk_nyc: https://t.co/JTCBLlAWdL\n\nCatch up with all the detoxes: https://t.co/rLG5L77FlI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			185
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "DLINQdigdetox",
+					"indices": [
+						19,
+						33
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "billk_nyc",
+					"name": "Bill Koulopoulos",
+					"id": 16519015,
+					"id_str": "16519015",
+					"indices": [
+						94,
+						104
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/JTCBLlAWdL",
+					"expanded_url": "https://dlinq.middcreate.net/digital-literacy/digital-detox-2-8-universal-design-for-learning-and-accessibility/",
+					"display_url": "dlinq.middcreate.net/digital-litera…",
+					"indices": [
+						106,
+						129
+					]
+				},
+				{
+					"url": "https://t.co/rLG5L77FlI",
+					"expanded_url": "https://dlinq.middcreate.net/tag/digitaldetox2/",
+					"display_url": "dlinq.middcreate.net/tag/digitaldet…",
+					"indices": [
+						162,
+						185
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 115986517,
+			"id_str": "115986517",
+			"name": "Sarah Lohnes Watulak",
+			"screen_name": "sarahlw6",
+			"location": "Vermont, USA",
+			"description": "critical digital pedagogy, inclusive design, connected learning. teacher/researcher/lifelong learner. director of digital pedagogy & media @ Middlebury. she/her",
+			"url": "https://t.co/OFWsSNGDRF",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/OFWsSNGDRF",
+							"expanded_url": "http://sarahlw.middcreate.net",
+							"display_url": "sarahlw.middcreate.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 306,
+			"friends_count": 262,
+			"listed_count": 2,
+			"created_at": "Sat Feb 20 18:10:50 +0000 2010",
+			"favourites_count": 3817,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1389,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/783687983580917760/Lu9Ckhid_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/783687983580917760/Lu9Ckhid_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/115986517/1510862487",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hot off the press! #DLINQdigdetox 2.8 - Universal Design for Learning &amp; Accessibility, by @billk_nyc: https://t.co/JTCBLlAWdL\n\nCatch up with all the detoxes: https://t.co/rLG5L77FlI"
+	},
+	{
+		"created_at": "Fri Feb 01 21:48:11 +0000 2019",
+		"id": 1091453182650339300,
+		"id_str": "1091453182650339329",
+		"full_text": "@Autumm Woohoo, congrats!",
+		"truncated": false,
+		"display_text_range": [
+			8,
+			25
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Autumm",
+					"name": "Autumm Caines is a Liminal Space",
+					"id": 6292182,
+					"id_str": "6292182",
+					"indices": [
+						0,
+						7
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1091452774078955500,
+		"in_reply_to_status_id_str": "1091452774078955520",
+		"in_reply_to_user_id": 6292182,
+		"in_reply_to_user_id_str": "6292182",
+		"in_reply_to_screen_name": "Autumm",
+		"user": {
+			"id": 33972737,
+			"id_str": "33972737",
+			"name": "Mo Pelzel",
+			"screen_name": "MorrisPelzel",
+			"location": "Grinnell, Iowa",
+			"description": "Director of Academic Technology @GrinnellCollege | online theology prof @Creighton | Catholic | husband | dad | runner | cyclist | cantor | puzzle/games geek",
+			"url": "https://t.co/HifqlQpaOV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/HifqlQpaOV",
+							"expanded_url": "http://morrispelzel.com",
+							"display_url": "morrispelzel.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 689,
+			"friends_count": 1099,
+			"listed_count": 38,
+			"created_at": "Tue Apr 21 17:29:45 +0000 2009",
+			"favourites_count": 6379,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 7683,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/428212615102226432/HN8-wYRh_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/428212615102226432/HN8-wYRh_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/33972737/1431025149",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@Autumm Woohoo, congrats!"
+	},
+	{
+		"created_at": "Fri Feb 01 21:00:11 +0000 2019",
+		"id": 1091441102027149300,
+		"id_str": "1091442146471751688",
+		"full_text": "The Grinnell College Garden gives students an intimate, hands-on experience with local food and sustainable agriculture. Episode 6 of All Things Grinnell digs into how the garden is putting sustainability into action. https://t.co/WIR6xvmiQy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			241
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/WIR6xvmiQy",
+					"expanded_url": "http://fal.cn/rcfT",
+					"display_url": "fal.cn/rcfT",
+					"indices": [
+						218,
+						241
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://www.falcon.io\" rel=\"nofollow\">Falcon Social Media Management </a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 17652635,
+			"id_str": "17652635",
+			"name": "Grinnell College",
+			"screen_name": "GrinnellCollege",
+			"location": "Grinnell, Iowa",
+			"description": "The official Twitter account for Grinnell College. Updates on news, faculty, students, athletics and campus life at #GrinnellCollege.",
+			"url": "http://t.co/cT1hUUMKcV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/cT1hUUMKcV",
+							"expanded_url": "http://www.grinnell.edu",
+							"display_url": "grinnell.edu",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 6107,
+			"friends_count": 560,
+			"listed_count": 217,
+			"created_at": "Wed Nov 26 16:07:19 +0000 2008",
+			"favourites_count": 4297,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4750,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/991321404195856384/WY7ENUeS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/991321404195856384/WY7ENUeS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/17652635/1402595600",
+			"profile_link_color": "B40012",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "E0E8F0",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The Grinnell College Garden gives students an intimate, hands-on experience with local food and sustainable agriculture. Episode 6 of All Things Grinnell digs into how the garden is putting sustainability into action. https://t.co/WIR6xvmiQy"
+	}
+]
